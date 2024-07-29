09db0a12334aa4cae8cac30b6c2c2e8daeaf9d04
LOKESH <lokeshkannarajaram@gmail.com>
2024-07-29 15:21:46 +0530

    data validation completed

--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -12,4 +12,14 @@ class DataIngestionConfig:
 
 
 
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir : Path
+    STATUS_FILE : str
+    ALL_REQUIRED_FILES: list
+
+
+
+
     
--- src/textSummarizer/pipeline/stage_01_data_ingestion.py
+++ src/textSummarizer/pipeline/stage_01_data_ingestion.py
@@ -1,6 +1,7 @@
 from textSummarizer.config.configuration import ConfigurationManager
-from textSummarizer import DataIngestion
+from textSummarizer.components.data_ingestion import DataIngestion
 from textSummarizer.logging import logger
+
 
 
 class DataIngestionTrainingPipeline:
